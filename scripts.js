8028028810e10408068b0d63e065e92e3c18d7e9
Helen Miles <hem23@aber.ac.uk>
2022-02-02 17:37:24 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ function startTime() {
   let s = today.getSeconds();
   m = checkTime(m);
   s = checkTime(s);
+  
   // calls
   jdut = julianDate(today.getTime());
   jdtt = julianTT(jdut);
@@ -44,4 +45,23 @@ function marsMeanAnomaly(dt) {
 
 function alphaFMS(dt){
  return (270.3871 + 0.524038496 * dt) % 360
+}
+
+function perturbers(dt) {
+  //PBS = Σ(i=1,7) Ai cos [ (0.985626° ΔtJ2000 / τi) + φi]
+  return 0.0071 * cos((0.985626 * dt /  2.2353) +  49.409) +
+    0.0057 * cos((0.985626 * dt /  2.7543) + 168.173) +
+    0.0039 * cos((0.985626 * dt /  1.1177) + 191.837) +
+    0.0037 * cos((0.985626 * dt / 15.7866) +  21.736) +
+    0.0021 * cos((0.985626 * dt /  2.1354) +  15.704) +
+    0.0020 * cos((0.985626 * dt /  2.4694) +  95.528) +
+    0.0018 * cos((0.985626 * dt / 32.8493) +  49.095)
+}
+
+function cos(deg) {
+  return Math.cos(deg * Math.PI / 180);
+}
+
+function sin(deg) {
+  return Math.sin(deg * Math.PI / 180);
 }
