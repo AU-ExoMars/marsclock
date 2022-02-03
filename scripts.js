ac56ea00b7224109f632d4b14d18f6557d6c75fb
Helen Miles <hem23@aber.ac.uk>
2022-02-03 10:45:33 +0000

    added very handy hh:mm:ss function from jtauber's marsclock

--- scripts.js
+++ scripts.js
@@ -17,6 +17,7 @@ function startTime() {
   eqocentre = eoc(dtj2000,mma,pbs)
   Ls = aFMS + eqocentre
   eotdeg = eotDeg(Ls,eqocentre)
+  eothrs = eotHrs(eotdeg)
   
   // prints
   document.getElementById('currentTime').innerHTML =  h + ":" + m + ":" + s;
@@ -82,4 +83,16 @@ function cos(deg) {
 
 function sin(deg) {
   return Math.sin(deg * Math.PI / 180);
+}
+
+function h_to_hms(h) {
+  var x = h * 3600;
+  var hh = Math.floor(x / 3600);
+  if (hh < 10) hh = "0" + hh;
+  var y = x % 3600;
+  var mm = Math.floor(y / 60);
+  if (mm < 10) mm = "0" + mm;
+  var ss = Math.round(y % 60);
+  if (ss < 10) ss = "0" + ss;
+  return hh + ":" + mm + ":" + ss;
 }
