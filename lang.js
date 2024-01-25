94f20fa73b6a7ac784f35087221ccf553fe73443
Helen Miles <hem23@aber.ac.uk>
2024-01-25 13:13:31 +0000

    Update lang.js

--- lang.js
+++ lang.js
@@ -14,6 +14,8 @@ var launchYmdEn = "Launch window opens in " + d_to_ymd(daysUntilLaunch, langs[0]
 var landingYmdEn = d_to_ymd(daysUntilLanding, langs[0]) + " until mission starts";
 var launchDaysEn = "Launch window opens in " + daysUntilLaunch + " days";
 var landingDaysEn = daysUntilLanding + " days until mission starts";
+var mtctagEn = "MTC (Mars)";
+var utctagEn = "UTC (Earth)";
 
 // Cy Strings
 var titleCy = "Cenhadaeth ExoMars Rosalind Franklin";
@@ -21,11 +23,15 @@ var launchYmdCy = "Ffenestr lansio yn agor mewn " + d_to_ymd(daysUntilLaunch, la
 var landingYmdCy = d_to_ymd(daysUntilLanding, langs[1]) + " tan ddechrau'r cenhadaeth";
 var launchDaysCy = "Ffenestr lansio yn agor mewn " + daysUntilLaunch + " dyddiau";
 var landingDaysCy = daysUntilLanding + " dydd tan ddechrau'r cenhadaeth";
+var mtctagCy = "MTC (Mawrth)";
+var utctagCy = "UTC (Y Ddaear)";
 
 // set-lang specific strings, default to En in case of an issue
 function setStrings(currentLang = langs[0]){
   if(currentLang == langs[0]){
     document.getElementById('title').innerHTML = titleEn;
+    document.getElementById('utcCap').innerHTML = utctagEn;
+    document.getElementById('mtcCap').innerHTML = mtctagEn;
     launchYmd = launchYmdEn;
     landingYmd = landingYmdEn;
     launchD = launchDaysEn;
@@ -33,6 +39,12 @@ function setStrings(currentLang = langs[0]){
   }
   if(currentLang == langs[1]){
     document.getElementById('title').innerHTML = titleCy;
+    document.getElementById('utcCap').innerHTML = utctagCy;
+    document.getElementById('mtcCap').innerHTML = mtctagCy;
+    launchYmd = launchYmdCy;
+    landingYmd = landingYmdCy;
+    launchD = launchDaysCy;
+    landingD = landingDaysCy;
   }
 
   if(dayFormatYMD){
