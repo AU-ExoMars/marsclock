57a0427cdca7a5a2af5ef09364fc73931f5bd7c4
Helen Miles <hem23@aber.ac.uk>
2024-01-25 12:08:47 +0000

    Update lang.js

--- lang.js
+++ lang.js
@@ -6,17 +6,41 @@ function toggleLang(currentLang){
   if(currentLang == langs[1]){return langs[0]};
 }
 
+var launchYmd, landingYmd, launchD, landingD;
+
 // En Strings
-titleEn = "ExoMars Rosalind Franklin Mission";
+var titleEn = "ExoMars Rosalind Franklin Mission";
+var launchYmdEn = "Launch window opens in " + d_to_ymd(daysUntilLaunch);
+var landingYmdEn = d_to_ymd(daysUntilLanding) + " until mission starts";
+var launchDaysEn = "Launch window opens in " + daysUntilLaunch + " days";
+var landingDaysEn = daysUntilLanding + " days until mission starts";
 
 // Cy Strings
-titleCy = "Cenhadaeth ExoMars Rosalind Franklin";
+var titleCy = "Cenhadaeth ExoMars Rosalind Franklin";
 
+// set-lang specific strings
 function setStrings(currentLang){
   if(currentLang == langs[0]){
     document.getElementById('title').innerHTML = titleEn;
+    launchYmd = launchYmdEn;
+    landingYmd = landingYmdEn;
+    launchD = launchDaysEn;
+    landingD = landingDaysEn;
   }
   if(currentLang == langs[1]){
     document.getElementById('title').innerHTML = titleCy;
   }
 }
+
+if(dayFormatYMD){
+  document.getElementById('landing').innerHTML = landingYmd;
+  document.getElementById('launch').innerHTML = launchYmd;
+} else {
+  document.getElementById('landing').innerHTML = landingD;
+  document.getElementById('launch').innerHTML = launchD;
+}
+
+// non-lang specific
+document.getElementById('earthUTC').innerHTML =  h + ":" + m + ":" + s;
+document.getElementById('marsCT').innerHTML = h_to_hms(mstPM);
+document.getElementById('ltstOP').innerHTML = h_to_hms(within_24(ltst));
