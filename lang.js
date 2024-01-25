c67605261074d75197e0134f74e6f627e678a5c5
Helen Miles <hem23@aber.ac.uk>
2024-01-25 11:38:22 +0000

    basic lang test

--- lang.js
+++ lang.js
@@ -5,3 +5,16 @@ function toggleLang(currentLang){
   if(currentLang == lang[0]){return lang[1]};
   if(currentLang == lang[1]){return lang[0]};
 }
+
+// En Strings
+titleEn = "ExoMars Rosalind Franklin Mission";
+
+// Cy Strings
+titleCy = "Cenhadaeth ExoMars Rosalind Franklin";
+
+if(currentLang == lang[0]){
+  document.getElementById('title').innerHTML = titleEn;
+}
+if(currentLang == lang[1]){
+  document.getElementById('title').innerHTML = titleCy;
+}
